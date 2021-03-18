7607b7e633793bcccfe561818ebeb95c4b75e91b
Ion Ceban <ceban.ion1992@gmail.com>
2021-03-18 17:33:42 -0500

    Initial Commit

---
+++ MemeME 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeME 1.0
+//
+//  Created by Ion Ceban on 3/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
