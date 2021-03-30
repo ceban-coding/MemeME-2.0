0458d3e2dce823d9ef8e28ac73416b33cd99542e
Ion Ceban <ceban.ion1992@gmail.com>
2021-03-30 16:56:11 -0500

    added picker delegate

---
+++ MemeME 1.0/LaunchViewCOntroller.swift
@@ -0,0 +1,8 @@
+//
+//  LaunchViewCOntroller.swift
+//  MemeME 1.0
+//
+//  Created by Ion Ceban on 3/18/21.
+//
+
+import Foundation
--- MemeME 1.0/ViewController.swift
+++ MemeME 1.0/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
+    
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var bottomToolbar: UIToolbar!
+    @IBOutlet weak var photoAlbumButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+
+   
+
+    // pickAnImageFromLibrary Button
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+        
+    
+       
+    
+    // pickAnImageFromCamera Button
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let cameraController = UIImagePickerController()
+        cameraController.delegate = self
+        cameraController.sourceType = .camera
+        present(cameraController, animated: true, completion: nil)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[.originalImage] as? UIImage{
+                imagePickerView.image = image
+            }
+            dismiss(animated: true, completion: nil)
+         }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+           dismiss(animated: true, completion: nil)
+           }
+    
+
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
-}
-
+    
+    
+    }
+    
