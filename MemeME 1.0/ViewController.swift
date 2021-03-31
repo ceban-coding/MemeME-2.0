340f07287f0f261504ed60cf04cc6624ec078951
Ion Ceban <ceban.ion1992@gmail.com>
2021-03-30 21:33:26 -0500

    added tex field

--- MemeME 1.0/ViewController.swift
+++ MemeME 1.0/ViewController.swift
@@ -58,15 +58,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
            dismiss(animated: true, completion: nil)
            }
     
-
-    
+    //Textfields
+    func setTextField(_ textField: UITextField) {
+        let textAttributes : [NSAttributedString.Key : Any] = [
+            .strokeColor: UIColor.black,
+            .foregroundColor: UIColor.white,
+            .strikethroughColor: UIColor.white,
+            .font: UIFont(name: "HelveticaNeue-CondensedBold", size: 40)!,
+            .strokeWidth: -3.0
+        ]
+        textField.defaultTextAttributes = textAttributes
+        textField.adjustsFontSizeToFitWidth = true
+        textField.textAlignment = .center
+        textField.allowsEditingTextAttributes = true
+    }
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.topTextField.delegate = self
+        self.bottomTextField.delegate = self
+        
+        self.setTextField(self.topTextField)
+        self.setTextField(self.bottomTextField)
+        
     }
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
+    
     
     
     }
